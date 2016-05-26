7f9b6b9ee4856b2550b81fa690acd135bbf7b826
Scott Harvey <scottyharvey33@gmil.com>
2016-05-26 16:43:46 -0600

    iTunes clone first

--- js/itunesService.js
+++ js/itunesService.js
@@ -1,12 +1,24 @@
 angular.module('itunes').service('itunesService', function($http, $q){
-  //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
-  //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
+  this.findIt = function(artist){
+    var deferred = $q.defer();
+      $http({
+      method: "JSONP",
+      url: "https://itunes.apple.com/search?term=" + artist + '&callback=JSON_CALLBACK',
+    }).then(function(response){
+      var data = response.data.results;
+      deferred.resolve(data);
+    })
+      return deferred.promise;
+}
+
+
+
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+
 });
